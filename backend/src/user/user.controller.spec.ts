d8458b2d9f6ac8d637a2a818f2617e462e9cb768
patrick-miharisoa <tojoniaina.patrick@futurmap.com>
2023-08-22 11:34:05 +0300

    added user.controller tests

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,20 +1,77 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+import { ResetPasswordService } from '../auth/reset-password.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
+  let prisma: PrismaService;
+  let configService: ConfigService;
+  let codeService: ResetPasswordService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-      providers: [UserService],
-    }).compile();
+  const users = [
+    {
+      id: '0',
+      email: 'user1@mail.com',
+      password: '0000',
+      fullname: 'user 1',
+    },
+    {
+      id: '1',
+      email: 'user2@mail.com',
+      password: '1111',
+      fullname: 'user 2',
+    },
+  ];
 
-    controller = module.get<UserController>(UserController);
+  beforeAll(async () => {
+    configService = new ConfigService();
+    prisma = new PrismaService({
+      datasources: {
+        db: {
+          url: configService.get('DATABASE_URL'),
+        },
+      },
+    });
+    userService = new UserService(prisma);
+    codeService = new ResetPasswordService(prisma);
+    controller = new UserController(userService, codeService, configService);
+
+    await prisma.user.deleteMany();
+    for (const user of users) {
+      await prisma.user.create({
+        data: user,
+      });
+    }
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('get one user', async () => {
+    const user = await controller.getUser(users[0].id);
+    expect(user).toBeDefined();
+  });
+
+  it('should set user password', async () => {
+    jest.spyOn(codeService, 'validateCode').mockReturnValue(true);
+    jest
+      .spyOn(codeService, 'getAssociatedEmail')
+      .mockReturnValue(users[0].email);
+
+    await controller.setPassword({
+      code: 'fake code',
+      password: 'new password',
+    });
+
+    const user = await prisma.user.findUnique({
+      where: { id: users[0].id },
+    });
+
+    expect(bcrypt.compare('new password', user.password)).resolves.toBeTruthy();
   });
 });
