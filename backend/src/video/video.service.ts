77f431e39c8af6e8648540d4e23d8a24a8678ffb
acf-patrick <miharisoap@gmail.com>
2023-09-25 11:15:14 +0300

    update

--- backend/src/video/video.service.ts
+++ backend/src/video/video.service.ts
@@ -141,7 +141,7 @@ export class VideoService {
           `./public/datas/videos/${video}`,
         );
         
-        console.log(videoPath);
+        console.error(videoPath);
         
         const videoInfo = statSync(videoPath);
         const CHUNK_SIZE = 1e6; // 1Mb
