306eca932c01e914a0019cfd03485596d0920422
acf-patrick <miharisoap@gmail.com>
2023-09-25 10:43:56 +0300

    added test endpoint in video.controller

--- backend/src/video/video.controller.ts
+++ backend/src/video/video.controller.ts
@@ -25,6 +25,8 @@ import { PrismaClientExceptionFilter } from '../prisma-client-exception/prisma-c
 import { AccessTokenGuard } from '../auth/guards/acces-token.guard';
 import { Request } from 'express';
 import { PrismaService } from '../prisma/prisma.service';
+import * as fs from 'fs';
+import { join } from 'path';
 
 @Controller('/api/v1/video')
 @UseFilters(PrismaClientExceptionFilter)
@@ -33,6 +35,11 @@ export class VideoController {
     private readonly videoService: VideoService,
     private prisma: PrismaService,
   ) {}
+
+  @Get('list')
+  listVideos() {
+    return fs.readdirSync(join(__dirname, '../..', 'public/datas/videos'));
+  }
 
   @Get('/a')
   @UseGuards(AccessTokenGuard)
